99ef89f4e2510026e2b93f1b08b94a3567598530
pal4de <pal4de@gmail.com>
2021-02-28 15:25:37 +0900

    geopackageからの読み込みに対応

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 // source: https://day-journal.com/memo/try-054/
-// todo: ダミーデータを用意し、そこに任意のマーカー (消火栓など) を表示できるかを調査
 
 const attribution = `
     <a href="https://maptiler.jp/" target="_blank">&copy; MIERUNE</a>
@@ -12,27 +11,22 @@ const m_streets = new L.tileLayer(
         attribution: attribution
     }
 );
-
-const m_gray = new L.tileLayer(
-    `https://api.maptiler.com/maps/jp-mierune-gray/256/{z}/{x}/{y}.png?key=${API_KEY}`,
-    {
-        attribution: attribution
-    }
-);
-
 const map = L.map('map', {
-    center: [32.80953813626903, 130.71637940099387],
-    zoom: 12,
+    center: [32.805100137858226, 130.77487836209724],
+    zoom: 14,
     zoomControl: true,
     layers: [m_streets],
 });
 
-const Map_BaseLayer = {
-    'MIERUNE Streets': m_streets,
-    'MIERUNE Gray': m_gray,
-};
+/*
+ToDo:
 
-L.control.layers(Map_BaseLayer, null).addTo(map);
+- 避難所
+- 断水エリア
+- 人口メッシュ
+
+レイヤの切り替え
+*/
 
 L.control
     .scale({
@@ -41,30 +35,57 @@ L.control
     })
     .addTo(map);
 
-// // GeoPackage読み込み
-// L.geoPackageFeatureLayer([], {
-//     // GeoPackageファイル指定
-//     geoPackageUrl: './data/shop.gpkg',
-//     // レイヤ名指定
-//     layerName: 'shop',
-//     pointToLayer: function (feature, layer) {
-//         return L.circleMarker(layer, {
-//             color: '#014c86',
-//             radius: 3,
-//             weight: 1,
-//             opacity: 0.7,
-//             fill: true,
-//             fillColor: '#014c86',
-//             fillOpacity: 0.7
-//         });
-//     },
-//     onEachFeature: function (feature, layer) {
-//         const field =
-//             `fid: ${feature.properties.fid} <br>
-//             full_id: ${feature.properties.full_id} <br>
-//             osm_id: ${feature.properties.osm_id} <br>
-//             name: ${feature.properties.name} <br>
-//             shop: ${feature.properties.shop}`;
-//         layer.bindPopup(field);
-//     }
-// }).addTo(map);
+const IconTemplate = L.Icon.extend({
+    options: {
+        iconSize: [15, 15],
+        shadowSize: [0, 0],
+        iconAnchor: [7.5, 7.5],
+        shadowAnchor: [0, 0],
+        popupAnchor: [7.5, 0]
+    }
+});
+const ShokasenIcon = new IconTemplate({iconUrl: './icons/shokasen.png'});
+const KyusuishaIcon = new IconTemplate({iconUrl: './icons/kyusuisha.png'});
+const HinanjoIcon = new IconTemplate({iconUrl: './icons/hinanjo.png'});
+
+const hinanjyoLayer = L.geoPackageFeatureLayer([], {
+    geoPackageUrl: './data/db.gpkg',
+    layerName: 'hinanjyo',
+    pointToLayer: function (feature, layer) {
+        return L.marker(layer, {icon: HinanjoIcon});
+    }
+});
+
+const dansuiAreaLayer = L.geoPackageFeatureLayer([], {
+    geoPackageUrl: './data/db.gpkg',
+    layerName: 'dansui_area',
+});
+
+const poplationLayer = L.geoPackageFeatureLayer([], {
+    geoPackageUrl: './data/db.gpkg',
+    layerName: '500m_mesh_2018_43',
+    style: (geoJsonFeature) => {
+        const population = geoJsonFeature.properties.ptn_2020;
+        const color = population < 770  ? '#fef0d9' :
+                      population < 1540 ? '#fdcc8a' :
+                      population < 2311 ? '#fc8d59' :
+                      population < 3081 ? '#e34a33' :
+                                          '#b30000' ;
+        return {
+            color: '#ffffff',
+            weight: 1,
+            fillColor: color,
+            fillOpacity: 0.5,
+        }
+    }
+});
+
+//背景レイヤ
+const optionLayersList = {
+    '避難所': hinanjyoLayer,
+    '断水エリア': dansuiAreaLayer,
+    '人口': poplationLayer,
+};
+
+//レイヤ設定
+L.control.layers(null, optionLayersList).addTo(map);
